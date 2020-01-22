92b8ba7d86e1f8fdb70302362b38d27e7a2d3d0f
Sharvanand <thirusharwan@gmail.com>
2020-01-21 20:16:22 -0500

    Second Commit

---
+++ src/components/todos.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Todos = ({ todos, deleteTodo }) => {
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            {todo.content}
+          </span>
+        </div>
+      );
+    })
+  ) : (
+    <p>There is no more Todos here</p>
+  );
+  return <div className="center collection">{todoList}</div>;
+};
+
+export default Todos;
